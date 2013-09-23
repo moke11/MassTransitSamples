97f1c2ac546c3ceab3dff4b1623fadf3cdd6e1ce
moke11 <mokror01@yblapdev09.corp.ybusa.net>
2013-09-23 11:36:02 -0400

    added interface to a message

---
+++ Common/Interfaces/IAddCustomerMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Interfaces
+{
+    public interface IAddCustomerMessage
+    {
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string PublishedDateTime { get; set; }
+    }
+}
---
+++ Common/Messages/AddCustomerMessage.cs
@@ -0,0 +1,11 @@
+﻿using Common.Interfaces;
+
+namespace Common.Messages
+{
+    public class AddCustomerMessage : IAddCustomerMessage
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PublishedDateTime { get; set; }
+    }
+}
